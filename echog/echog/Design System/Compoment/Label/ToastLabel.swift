dc48b52f6db1fb6ee41eb575b128453221fe37a9
mint3382 <mskim0926@naver.com>
2024-09-19 11:28:27 +0900

    ✨(feat) ToastLabel, Protocol 구현

---
+++ echog/echog/Design System/Compoment/Label/ToastLabel.swift
@@ -0,0 +1,61 @@
+//
+//  ToastLabel.swift
+//  echog
+//
+//  Created by minsong kim on 9/19/24.
+//
+
+import UIKit
+import SnapKit
+
+class ToastLabel: UIView {
+    let iconImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        
+        return view
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.font = .mediumBody2
+        label.textColor = .primary30
+        
+        return label
+    }()
+    
+    convenience init(icon image: UIImage, text: String) {
+        self.init()
+        self.backgroundColor = .grayscale100
+        self.layer.cornerRadius = 10
+        self.messageLabel.text = text
+        self.iconImageView.image = image.resize(newWidth: 20).changeImageColor(.primary30)
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        configureIconView()
+        configureLabel()
+    }
+    
+    private func configureIconView() {
+        self.addSubview(iconImageView)
+        
+        iconImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.height.width.equalTo(20)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    private func configureLabel() {
+        self.addSubview(messageLabel)
+        
+        messageLabel.snp.makeConstraints { make in
+            make.leading.equalTo(iconImageView.snp.trailing).offset(8)
+            make.trailing.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview()
+        }
+    }
+}
---
+++ echog/echog/Design System/Compoment/ToastProtocol.swift
@@ -0,0 +1,43 @@
+//
+//  ToastProtocol.swift
+//  echog
+//
+//  Created by minsong kim on 9/19/24.
+//
+
+import UIKit
+import SnapKit
+
+protocol ToastProtocol where Self: UIViewController {
+    var window: UIWindow? { get set }
+}
+
+extension ToastProtocol {
+    func showToast(icon: UIImage, message: String) {
+        let toastLabel = ToastLabel(icon: icon, text: message)
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return
+        }
+        window = UIWindow(windowScene: windowScene)
+        window?.windowLevel = .alert
+        window?.isUserInteractionEnabled = false
+        window?.addSubview(toastLabel)
+        
+        toastLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(60)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().inset(100)
+        }
+        
+        self.window?.isHidden = false
+        UIView.animate(withDuration: 1.0, delay: 1.5, options: .curveEaseOut, animations: {
+            self.window?.alpha = 0.0
+        }) { _ in
+            self.window?.alpha = 1.0
+            self.window?.isHidden = true
+        }
+    }
+}
